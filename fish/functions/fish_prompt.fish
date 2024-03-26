f87e6653d453cae9cd7f6e3b1aa1461a03691b75
Ryan Sullivan <ryan@sul.li>
2024-03-26 15:59:09 -0700

    ✨ Update fish shell prompt display for Git and Ruby info
    - Update the fish prompt to display Git and Ruby information in a more organized and visually appealing way.
    - Add support for displaying the current Git branch and Ruby version in the prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,44 +1,77 @@
-if test $TERM_PROGRAM = "WarpTerminal"
-  function fish_prompt
-    set last_status $status
+function fish_prompt
+  set last_status $status
 
-    set_color cyan
-    echo -n (prompt_pwd)" "
+  if test $TERM_PROGRAM != "WarpTerminal"
+    echo ""
+  end
 
-    # Git prompt
-    set_color yellow
-    echo -n "["(git config user.email)":"
-    __fish_git_prompt "%s]"
+  set_color cyan
+  echo -n (prompt_pwd)" "
 
-    # rbenv version
-    set_color red
-    echo -n "["(rbenv version-name)"]"
 
-    # Print command duration
-    if test $CMD_DURATION; and test $CMD_DURATION != 0
-      set duration $CMD_DURATION
+  __git_prompt
+  __ruby_prompt
 
-      set_color green
+  if test $TERM_PROGRAM != "WarpTerminal"
+    echo ""
+  end
 
-      # Show duration of the last command in seconds
-      if test $CMD_DURATION -gt 1000
-        set_color yellow
-        set duration (echo "$duration 1000" | awk '{printf "%.3fs", $1 / $2}')
-      end
+  __duration_prompt
 
-      if test $CMD_DURATION -gt 10000
-        set_color red
-      end
-
-      echo -n "[$duration]"
-    end
-
-    set_color red
+  set_color red
+  if test $TERM_PROGRAM = "WarpTerminal"; and test $last_status != 0
+    echo -n "!"
+  else
     switch $last_status
     case 0
       set_color green
     end
-    echo -n "\$ "
-    set_color normal
+    echo -n "❯ "
+  end
+
+  set_color normal
+end
+
+function __ruby_prompt
+  # rbenv version
+  set_color red
+  set global_ruby (rbenv global)
+  set local_ruby (rbenv version-name)
+  if test "$global_ruby" != "$local_ruby"
+    echo -n "[ "(rbenv version-name)"]"
+  end
+end
+
+function __git_prompt
+  # Git prompt
+  set_color yellow
+  set -g __fish_git_prompt_show_informative_status true
+  set -g __fish_git_prompt_showcolorhints true
+  set -g __fish_git_prompt_color yellow
+  set -g __fish_git_prompt_color_branch yellow
+  set -g __fish_git_prompt_color_upstream green
+
+  echo -n "[ "(git config user.email)"|"
+  fish_git_prompt "%s]"
+end
+
+function __duration_prompt
+  # Print command duration
+  if test $CMD_DURATION; and test $CMD_DURATION != 0
+    set duration $CMD_DURATION
+
+    set_color green
+
+    # Show duration of the last command in seconds
+    if test $CMD_DURATION -gt 1000
+      set_color yellow
+      set duration (echo "$duration 1000" | awk '{printf "%.3fs", $1 / $2}')
+    end
+
+    if test $CMD_DURATION -gt 10000
+      set_color red
+    end
+
+    echo -n "[$duration]"
   end
 end
