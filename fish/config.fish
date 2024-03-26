9315f34e36c62c1c599a217440799cf4a426114e
Ryan Sullivan <ryan@sul.li>
2024-03-26 16:00:29 -0700

    ✨ Update path settings and sources in config.fish
    - Updated paths to use Homebrew directories for openjdk and libpq binaries.
    - Changed the autojump source to Homebrew directory.
    - Set fish cursor defaults to improve visibility of modes.

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
 set fish_greeting ""
 
 # Path Settings
-fish_add_path /usr/local/sbin
-fish_add_path /usr/local/opt/openjdk/bin
+fish_add_path /opt/homebrew/sbin
+fish_add_path /opt/homebrew/opt/openjdk/bin
+fish_add_path /opt/homebrew/opt/libpq/bin
 fish_add_path $HOME/.rbenv/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $GOPATH/bin
@@ -10,23 +11,22 @@ fish_add_path $GOPATH/bin
 # Golang Settings
 set -gx GOPATH $HOME/src/go
 
-# GPG Settings
-set -gx GPG_TTY (tty)
-gpgconf --launch gpg-agent
-
 # Use 1password for ssh key management
 set -gx SSH_AUTH_SOCK $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
 # Load addtional apps
-test -e $HOME/.iterm2_shell_integration.fish ; and source $HOME/.iterm2_shell_integration.fish
-test -e /usr/local/share/autojump/autojump.fish ; and source /usr/local/share/autojump/autojump.fish
+test -e /opt/homebrew/share/autojump/autojump.fish ; and source /opt/homebrew/share/autojump/autojump.fish
 
 set -gx EDITOR nvim
 set -gx GIT_EDITOR nvim
 set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --no-ignore-vcs'
 
-# Use vim key bindings and clear the mode prompt since it's included in the right prompt
+# Use vim key bindings and clear the mode prompt since the cursor should indicate the mode
 fish_vi_key_bindings
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
 function fish_mode_prompt; end
 
 # Load additional settings
