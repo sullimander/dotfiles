edfb798512e9f744f3aaa611e74818df3a6e5ec7
Ryan Sullivan <ryan@sul.li>
2024-03-26 16:05:10 -0700

    ✨ Update go-config.lua for Go metalinter configuration
    - Enable go_metalinter_autosave
    - Enable golint, vet, and errcheck for go_metalinter_enabled array

--- nvim/lua/go-config.lua
+++ nvim/lua/go-config.lua
@@ -4,5 +4,5 @@ vim.g.go_highlight_fields = 1
 vim.g.go_highlight_function_calls = 1
 vim.g.go_highlight_functions = 1
 vim.g.go_highlight_types = 1
--- vim.g.go_metalinter_autosave = 1
--- vim.g.go_metalinter_enabled = { 'vet', 'golint', 'errcheck' }
+vim.g.go_metalinter_autosave = 1
+vim.g.go_metalinter_enabled = { 'vet', 'golint', 'errcheck' }
